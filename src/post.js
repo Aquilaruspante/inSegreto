51ac135f0006ac1fcbee8e95a2c0bce3fd5ab48f
Aquilaruspante <calogero.salvaggio@outlook.it>
2025-04-08 17:40:18 +0100

    feat(post): Add post.js with Post class

---
+++ src/post.js
@@ -0,0 +1,8 @@
+export default class Post {
+    constructor (gender, age, body, id) {
+        this.gender = gender;
+        this.age = age;
+        this.body = body;
+        this.id = id;
+    }
+}
