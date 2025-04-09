56ce8ff8c48e4f566f8559cd22a5563d1673f5e1
Aquilaruspante <calogero.salvaggio@outlook.it>
2025-04-09 21:15:04 +0100

    Add newPost form and listeners

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import { postLogger } from './postmanager.js';
+import { renderFeed, submitNewPost } from './postmanager.js';
+import storage from './storage.js';
 
 window.onload = () => {
     const main = document.querySelector('main');
     const newPostForm = document.querySelector('#new-post-btn');
+    const newPostDialog = document.querySelector('#new-post-dialog');
 
-    postLogger(main);    
+    const sayItForm = document.querySelector('#say-it-form');
+
+    newPostForm.addEventListener('submit', () => {
+        newPostDialog.showModal();
+    })
+
+    sayItForm.addEventListener('submit', () => {
+        submitNewPost();
+        renderFeed(main); 
+    })
+    //   
 }
