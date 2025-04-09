cb08eef5b6d345a4674a1b1d8531a6d29f30d85b
Aquilaruspante <calogero.salvaggio@outlook.it>
2025-04-09 19:51:29 +0100

    Feat(Post btn): Add button

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { postLogger } from './postmanager.js';
 
 window.onload = () => {
     const main = document.querySelector('main');
+    const newPostForm = document.querySelector('#new-post-btn');
 
     postLogger(main);    
 }
