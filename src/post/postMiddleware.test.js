438d8bab685d28d86481a6d667fb59bf02857947
Aquilaruspante <calogero.salvaggio@outlook.it>
2025-04-10 19:44:11 +0100

    feat(middleware): test suite for validatePost finished

--- src/post/postMiddleware.test.js
+++ src/post/postMiddleware.test.js
@@ -16,5 +16,41 @@ describe('validatePost function', () => {
         age = 23;
         body = ''
         expect(() => validatePost(gender, age, body)).toThrow('All fields must be filled!!!');
+
+        gender = 'Female';
+        age = 23;
+        body = 'dfsd'
+        expect(() => validatePost(gender, age, body)).not.toThrow();
+    });
+
+    test('it throws with gender different than Male, Female, Trans or Non-binary', () => {
+        let gender = 'Pippo';
+        let age = 33;
+        let body = 'cocco';
+        expect(() => validatePost(gender, age, body)).toThrow('Gender must be chosen among the available choices!!!');
+    });
+
+    test('it does not throw with Non-binary', () => {
+        let gender = 'Non-binary';
+        let age = 33;
+        let body = 'cocco';
+        expect(() => validatePost(gender, age, body)).not.toThrow();
+    })
+
+    test('it throws with age less than 13 or more that 99 but not with age in range', () => {
+        let gender = 'Non-binary';
+        let age = 3;
+        let body = 'cocco';
+        expect(() => validatePost(gender, age, body)).toThrow('Age must be between 13 and 99!!!');
+
+        gender = 'Non-binary';
+        age = 103;
+        body = 'cocco';
+        expect(() => validatePost(gender, age, body)).toThrow('Age must be between 13 and 99!!!');
+
+        gender = 'Non-binary';
+        age = 43;
+        body = 'cocco';
+        expect(() => validatePost(gender, age, body)).not.toThrow();
     });
 });
