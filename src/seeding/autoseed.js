5ba2b48181d4bca162ef6094646f595bc56da2f2
Aquilaruspante <calogero.salvaggio@outlook.it>
2025-04-16 19:37:43 +0100

    feat(autoseeding): add createRandomPost function

--- src/seeding/autoseed.js
+++ src/seeding/autoseed.js
@@ -1,7 +1,16 @@
 import text from "./text.js";
+import storage from "../storage/storage.js";
+
+function createRandomPost() {
+    const start = Math.floor(Math.random() * (text.length - 200));
+    const postLength = Math.floor(Math.random() * 150) + 50;
+    const end = start + postLength;
+    
+    return text.slice(start, end);
+}
 
 function autoseed() {
-    console.log(text.length);
+    console.log(createRandomPost());
 }
 
 export { autoseed };
