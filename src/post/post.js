acad8f736c676e0bdb89f5d516944fb0a1bebda2
Aquilaruspante <calogero.salvaggio@outlook.it>
2025-04-18 14:45:04 +0100

    feat(date): add date attribute to Post class

--- src/post/post.js
+++ src/post/post.js
@@ -1,8 +1,11 @@
+import { format } from 'date-fns';
+
 export default class Post {
     constructor (gender, age, body, id) {
         this.gender = gender;
         this.age = age;
         this.body = body;
+        this.date = format(new Date.now(), 'dd/MM/yyyy');
         this.id = id;
     }
 }
