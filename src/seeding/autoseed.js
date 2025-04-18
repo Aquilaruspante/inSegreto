a34088cb41541ce3b14cf5e17c1fd078f40001d2
Aquilaruspante <calogero.salvaggio@outlook.it>
2025-04-18 12:12:18 +0100

    fix(autoseeding): posts start at the beginning of an actual word now

--- src/seeding/autoseed.js
+++ src/seeding/autoseed.js
@@ -2,7 +2,10 @@ import text from "./text.js";
 import storage from "../storage/storage.js";
 
 function createRandomPost() {
-    const start = Math.floor(Math.random() * (text.length - 200));
+    let start;
+    while (text[start - 1] !== ' ') {
+        start = Math.floor(Math.random() * (text.length - 200));
+    };
     const postLength = Math.floor(Math.random() * 150) + 50;
     const end = start + postLength;
     
